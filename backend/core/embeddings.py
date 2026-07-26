80b1a0b879e323a7a24db8fc37e4768b7293cf6e
kartikdalal19 <dalalkartik077@gmail.com>
2026-07-26 16:17:37 +0530

    Update embeddings.py

--- backend/core/embeddings.py
+++ backend/core/embeddings.py
@@ -1,53 +1,88 @@
-from sentence_transformers import SentenceTransformer
+
+from huggingface_hub import InferenceClient
 from core.config import settings
+import numpy as np
+
 
 
 class EmbeddingGenerator:
 
+
     def __init__(self, model_name=None):
 
         if model_name is None:
-            model_name = settings.EMBEDDING_MODEL
+            model_name = settings.HF_EMBEDDING_MODEL
 
-        print(f"Loading embedding model: {model_name}")
 
-        self.model = SentenceTransformer(
-            model_name,
-            device="cpu"
+        print(
+            f"Using HuggingFace Embedding API: {model_name}"
         )
+
+
+        self.client = InferenceClient(
+            token=settings.HF_TOKEN
+        )
+
+
+        self.model = model_name
+
 
 
     # ----------------------------------------
+    # Single text embedding
+    # ----------------------------------------
 
     def embed_text(self, text: str):
-        return self.model.encode(
+
+        vector = self.client.feature_extraction(
             text,
-            convert_to_numpy=True,
-            normalize_embeddings=True
+            model=self.model
         )
 
 
+        vector = np.array(
+            vector,
+            dtype="float32"
+        )
+
+
+        return vector
+
+
+
+    # ----------------------------------------
+    # Multiple chunks embedding
     # ----------------------------------------
 
     def embed_chunks(self, chunks: list):
 
+
         texts = [
-            chunk.text 
+            chunk.text
             for chunk in chunks
         ]
-
-        vectors = self.model.encode(
-            texts,
-            batch_size=4,          # important for 8GB RAM
-            convert_to_numpy=True,
-            normalize_embeddings=True,
-            show_progress_bar=False
-        )
 
 
         embedded_chunks = []
 
-        for chunk, vector in zip(chunks, vectors):
+
+        for chunk, text in zip(
+            chunks,
+            texts
+        ):
+
+
+            vector = self.client.feature_extraction(
+                text,
+                model=self.model
+            )
+
+
+            vector = np.array(
+                vector,
+                dtype="float32"
+            )
+
 
             embedded_chunks.append(
                 {
